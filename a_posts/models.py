3f1b2e6140e0493644e330f3dfb371599db8dd8c
kitindi <abdullkitindi@gmail.com>
2024-04-18 04:32:39 +0300

    added question model

--- a_posts/models.py
+++ a_posts/models.py
@@ -1,3 +1,18 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=300)
+    image = models.ImageField(upload_to="uploads", null=True, blank=True)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    id =models.CharField(max_length=100, default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering =['-created']
